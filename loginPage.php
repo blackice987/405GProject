f4c9d3f7d6a4bb191d1786ab8e37fcaa5a063668
Trent <trent.minch@yahoo.com>
2018-10-29 11:57:05 -0400

    Modified the homepage.php file from the lab class a little to form the start of a login page. Right now just has a spot for name

--- loginPage.php
+++ loginPage.php
@@ -1,1 +1,31 @@
-//Make the Login Page
+
+
+<html>
+<body>
+<Title>Create An Account</Title>
+
+<b>Enter your account information below</b> <br><br>
+
+<div class='container'>
+	<label for='name' > Name: </label><br/>
+	<input type='text' name='name' id='name' maxlength = "50" /><br/>
+
+</div>
+
+
+<br>
+<form action="Submit.php" method="post">
+<input type="submit" value="Create your Account">
+</form>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
