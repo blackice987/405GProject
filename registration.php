487bddc7060a16e1374763e6d84cec02ded98430
Trent <trent.minch@yahoo.com>
2018-10-31 10:59:38 -0400

    Renamed loginPage to registration

---
+++ registration.php
@@ -0,0 +1,36 @@
+
+
+<html>
+<body>
+<Title>Create An Account</Title>
+
+<b>Enter your account information below</b> <br><br>
+
+<div class='container'>
+	<label for='name' > Username: </label><br/>
+	<input type='text' name='name' id='name' maxlength = "50" /><br/>
+
+<div class='container'>
+        <label for='name' > Password:  </label><br/>
+        <input type='text' name='password' id='password' maxlength = "50" /><br/>
+
+
+</div>
+
+
+<br>
+<form action="Submit.php" method="post">
+<input type="submit" value="Create your Account">
+</form>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
