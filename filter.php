8c279e9849bb7fef8e0bac80dcca93203f1eecd9
Brian <blu226@g.uky.edu>
2018-12-05 13:22:42 -0500

    homepage stuff

---
+++ filter.php
@@ -0,0 +1,51 @@
+<?php
+
+
+$Pass = '1382.Booda.1101'; //enter password 
+$DB = 'VEXLeaderboards'; //Enter database name
+$mysqli = new mysqli('127.0.0.1', 'root',$Pass,$DB);
+
+
+// Check for connection error 
+// If there is an error we will use $mysqli->connect_error 
+// to print the cause of the error 
+if ($mysqli->connect_errno) {
+        echo "Could not connect to database \n";
+        echo "Error: ". $mysqli->connect_error . "\n";
+        exit;
+// $Use_DB_Query = "USE VEXLeaderboards;";
+}
+else{ 
+
+        $query = "SELECT * FROM Teams";
+
+        if ( !$q_result = $mysqli->query($query) ) {
+                        echo "Query failed: ". $mysqli->error. "\n";
+                        exit;
+        }
+        else{
+        	echo "<table style='width:100%'>";
+        	echo "<tr><th>Rank</th><th>Team Name</th><th>Win Points</th><th>Strength Points</th></tr>";
+
+                
+             
+
+                while($row = $q_result->fetch_assoc()){
+
+        		echo "<tr>";
+        		echo "<td>r</td><td>tn</td><td>wp</td><td>sp</td>";
+        		echo "</tr>";
+
+                }
+                
+
+        	echo "</table>";
+        }
+}
+
+
+
+?>
+
+
+
--- homepage.php
+++ homepage.php
@@ -4,16 +4,24 @@
 
 <b>VEX Leaderboards: Home</b>
 <br><br>
-Please select an action: <br><br>
+Please select an action:
+
+<br><br>
 
 <input type="button" name="Create" value="Create Your Account" onclick="window.location.href='http://128.163.232.243/registration.php'">
-<br>
+
 <input type="button" name="Sign In" value="Sign In to Your Account" onclick="window.location.href='http://128.163.232.243/loginPage.php'">
-<br>
-<input type="button" name="View" value="View the Leaderboard" onclick="window.location.href='http://128.163.232.243/leaderboard.php'">
-<br>
+
 <input type="button" name="Report" value="Report Match Results" onclick="window.location.href='http://128.163.232.243/report.php'">
-<br>
+<br><br>
+Filter by:
+<form action = "filter.php" method="get">
+<input type="submit" value="Filter">
+</form>
+<input type="button" name="Rank" value="Rank" onclick="window.location.href='http://128.163.141.190/registration.php'">
+<input type="button" name="Team" value="Team Name" onclick="window.location.href='http://128.163.141.190/registration.php'">
+<input type="button" name="Tournament" value="Tournament Name" onclick="window.location.href='http://128.163.141.190/registration.php'">
+
 
 
 </body>
